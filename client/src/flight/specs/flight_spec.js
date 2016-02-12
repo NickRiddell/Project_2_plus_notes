dea09e48fd45d3d871acf146dac98105ae4f707a
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-02-12 16:54:13 +0000

    Commit before merge

--- client/src/flight/specs/flight_spec.js
+++ client/src/flight/specs/flight_spec.js
@@ -9,8 +9,8 @@ describe('flight', function() {
     assert.equal(flight.arrival, "Melbourne");
   });
   it('should have departing date and arriving date', function() {
-    var flight = new Flight("Edinburgh", "Melbourne", new Date("28-03-2016 T08:00:00"), new Date("29-03-2016 T10:00:00"));
-    assert.equal(flight.departing, new Date("28-03-2016 T08:00:00"));
-    assert.equal(flight.arriving, new Date("29-03-2016 T10:00:00"));
+    var flight = new Flight("Edinburgh", "Melbourne", new Date("28-03-2016T08:00:00"), new Date("29-03-2016T10:00:00"));
+    assert.equal(flight.departing, new Date("28-03-2016T08:00:00"));
+    assert.equal(flight.arriving, new Date("29-03-2016T10:00:00"));
   });
 })
