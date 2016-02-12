009c1ad9ac72528764f3eedfe9bb58f67e93fcd4
oscarbrooks <oscarobrooko@googlemail.com>
2016-02-12 15:56:20 +0000

    pass first flight model test

--- client/src/flight/flight.js
+++ client/src/flight/flight.js
@@ -1,3 +1,12 @@
-// var Flight =
+var Flight = function(departure, arrival, departing, arriving, price){
+  this.departure = departure;
+  this.arrival = arrival;
+  this.departing = departing;
+  this.arriving = arriving;
+}
+
+Flight.prototype = {
+
+}
 
 module.exports = Flight;
--- client/src/flight/specs/flight_spec.js
+++ client/src/flight/specs/flight_spec.js
@@ -6,6 +6,6 @@ describe('flight', function() {
   it('should have departure and arrival locations', function() {
     var flight = new Flight("Edinburgh", "Melbourne");
     assert.equal(flight.departure, "Edinburgh");
-    assert.equal(flight.departure, "Melbourne");
+    assert.equal(flight.arrival, "Melbourne");
   });
 })
