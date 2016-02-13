9793484a4d638b5612a131149d3d686e0822e4fe
oscarbrooks <oscarobrooko@googlemail.com>
2016-02-13 12:41:31 +0000

    a

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -44,11 +44,22 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	var displayDropdown = function(nameList) {
+	var departureDisplayDropdown = function(nameList) {
 	  for (var i = 0; i < nameList.length; i++) {
 	    var option = document.createElement("option");
 	    option.innerText = nameList[i];
-	    var select = document.querySelector("select");
+	    var select = document.querySelector("select#departure");
+	    select.appendChild(option);
+	  }
+	}
+	
+	var arrivalDisplayDropdown = function(nameList) {
+	  for (var i = 0; i < nameList.length; i++) {
+	    var option = document.createElement("option");
+	    option.innerText = nameList[i];
+	    console.log(option.innerText);
+	    var select = document.querySelector("select#arrival");
+	    console.log(select);
 	    select.appendChild(option);
 	  }
 	}
@@ -57,12 +68,27 @@
 	  console.log('loaded');
 	  var url = '';
 	  var cityNameList = ["Edinburgh"];
+	  var arrivalCityNameList = ["Melbourne", "Canberra", "Sydney"];
 	  var departure_dropdown = document.getElementById('departure');
+	  var arrival_dropdown = document.getElementById('arrival');
 	
-	  displayDropdown(cityNameList);
+	  departureDisplayDropdown(cityNameList);
+	  arrivalDisplayDropdown(arrivalCityNameList);
 	
 	  departure_dropdown.onchange = function() {
-	    console.log('here');
+	    var cityName = this.value;
+	    var cityIndex = null;
+	
+	    for (index in cityNameList) {
+	      var testCityName = cityNameList[index];
+	      if (testCityName === cityName) {
+	        var cityIndex = index;
+	        console.log(cityName);
+	      }
+	    }
+	  }
+	
+	  arrival_dropdown.onchange = function() {
 	    var cityName = this.value;
 	    var cityIndex = null;
 	
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
-var displayDropdown = function(nameList) {
+var departureDisplayDropdown = function(nameList) {
   for (var i = 0; i < nameList.length; i++) {
     var option = document.createElement("option");
     option.innerText = nameList[i];
-    var select = document.querySelector("select");
+    var select = document.querySelector("select#departure");
+    select.appendChild(option);
+  }
+}
+
+var arrivalDisplayDropdown = function(nameList) {
+  for (var i = 0; i < nameList.length; i++) {
+    var option = document.createElement("option");
+    option.innerText = nameList[i];
+    console.log(option.innerText);
+    var select = document.querySelector("select#arrival");
+    console.log(select);
     select.appendChild(option);
   }
 }
@@ -11,12 +22,27 @@ window.onload = function(){
   console.log('loaded');
   var url = '';
   var cityNameList = ["Edinburgh"];
+  var arrivalCityNameList = ["Melbourne", "Canberra", "Sydney"];
   var departure_dropdown = document.getElementById('departure');
+  var arrival_dropdown = document.getElementById('arrival');
 
-  displayDropdown(cityNameList);
+  departureDisplayDropdown(cityNameList);
+  arrivalDisplayDropdown(arrivalCityNameList);
 
   departure_dropdown.onchange = function() {
-    console.log('here');
+    var cityName = this.value;
+    var cityIndex = null;
+
+    for (index in cityNameList) {
+      var testCityName = cityNameList[index];
+      if (testCityName === cityName) {
+        var cityIndex = index;
+        console.log(cityName);
+      }
+    }
+  }
+
+  arrival_dropdown.onchange = function() {
     var cityName = this.value;
     var cityIndex = null;
 
