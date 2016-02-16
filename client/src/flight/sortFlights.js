e2868dcd11827231a7fd97c873fdc0feb33fd9b3
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-02-16 13:50:16 +0000

    added sort flights by journey length function

--- client/src/flight/sortFlights.js
+++ client/src/flight/sortFlights.js
@@ -6,11 +6,37 @@ var sortFlights = function(flights, sortBy) {
     return flights;
   }
   else if (sortBy === "journey time") {
-    // hotels.sort(function(a, b) {
-    //  return b.stars-a.stars;
-    // })
-    // return flights;
-  };
+    flights.sort(function(a, b) {
+      var aStartTime = new Date(a.departing);
+      var aStartTimeUTC = aStartTime.getTime() + aStartTime.getTimezoneOffset()*60000;
+      var aEndTime = new Date(a.arriving);
+      var aEndTimeUTC = aEndTime.getTime() + aEndTime.getTimezoneOffset()*60000;
+      var aJourneyTime = aEndTimeUTC - aStartTimeUTC;
+      var aJourneyTimeHours = aJourneyTime/(60*60*1000);
+      console.log("a's journey time: ", aJourneyTimeHours);
+      var bStartTime = new Date(b.departing);
+      var bStartTimeUTC = bStartTime.getTime() + bStartTime.getTimezoneOffset()*60000;
+      var bEndTime = new Date(b.arriving);
+      var bEndTimeUTC = bEndTime.getTime() + bEndTime.getTimezoneOffset()*60000;
+      var bJourneyTime = bEndTimeUTC - bStartTimeUTC;
+      var bJourneyTimeHours = bJourneyTime/(60*60*1000);
+      console.log("b's journey time: ", bJourneyTimeHours);
+      return aJourneyTime-bJourneyTime;
+    })
+    return flights;
+  }
 }
 
 module.exports = sortFlights;
+
+// var flights=[];
+// flights[0]={name:"CCC", price:32, departing: "2016-03-28T08:00:00Z", arriving: "2016-03-29T10:00:00Z"};
+// flights[1]={name:"BBB", price:17, departing: "2016-03-28T12:00:00Z", arriving: "2016-03-29T13:00:00Z"};
+// flights[2]={name:"AAA", price:58, departing: "2016-03-28T10:00:00Z", arriving: "2016-03-29T11:00:00Z"};
+// flights[3]={name:"DDD", price:62, departing: "2016-03-28T04:00:00Z", arriving: "2016-03-29T06:00:00Z"};
+
+// var sorted = sortFlights(flights, "price");
+// console.log(sorted);
+// sorted = sortFlights(flights, "journey time");
+// console.log(sorted);
+
