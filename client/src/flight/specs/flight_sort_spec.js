388ba33e00ffcfbd744cbef3cbf30828d7d65c4d
oscarbrooks <oscarobrooko@googlemail.com>
2016-02-17 09:56:47 +0000

    fix the other flight_spec test files too

--- client/src/flight/specs/flight_sort_spec.js
+++ client/src/flight/specs/flight_sort_spec.js
@@ -1,16 +1,16 @@
   var Flight = require('../flight.js');
   var chai = require('chai');
   var assert = chai.assert;
-  var sortFlights = require('./sort_flights.js');
+  var sortFlights = require('../sortFlights.js');
 
   describe('flight', function() {
     it('should have the cheapest result first', function() {
       flights = [
-      flight1 = new Flight("Europe/London", "Australia/Melbourne", "Edinburgh", "Melbourne", "28-03-2016 T08:00:00", "29-03-2016 T10:00:00", null, 300),
+      flight1 = new Flight("Europe/London", "Australia/Melbourne", "Edinburgh", null, "Melbourne", null, "28-03-2016 T08:00:00", "29-03-2016 T10:00:00", null, 300),
 
-      flight2 = new Flight("Europe/London", "Australia/Melbourne", "Edinburgh", "Melbourne", "28-03-2016 T08:00:00", "29-03-2016 T10:00:00", null, 200),
+      flight2 = new Flight("Europe/London", "Australia/Melbourne", "Edinburgh", null, "Melbourne", null, "28-03-2016 T08:00:00", "29-03-2016 T10:00:00", null, 200),
 
-      flight3 = new Flight("Europe/London", "Australia/Melbourne", "Edinburgh", "Melbourne", "28-03-2016 T08:00:00", "29-03-2016 T10:00:00", null, 800),
+      flight3 = new Flight("Europe/London", "Australia/Melbourne", "Edinburgh", null, "Melbourne", null, "28-03-2016 T08:00:00", "29-03-2016 T10:00:00", null, 800),
       ];
 
       var priceSortedFlights = [flight2, flight1, flight3];
--- client/src/flight/specs/timezone_spec.js
+++ client/src/flight/specs/timezone_spec.js
@@ -5,7 +5,7 @@ var momentString = require('./../momentString.js');
 
 describe('momentString', function() {
   it('should take in a date string and return a correctly formatted string for the time zone', function() { 
-    var flight = new Flight("Europe/London", "Australia/Melbourne", "Edinburgh", "Melbourne", "28-03-2016 T08:00:00", "29-03-2016 T10:00:00", 248);
+    var flight = new Flight("Europe/London", "Australia/Melbourne", "Edinburgh", null, "Melbourne", null, "28-03-2016 T08:00:00", "29-03-2016 T10:00:00", 248);
     var convertedDate = momentString(flight.departing, flight.departureTZ);
     assert.equal(convertedDate, "March 28th 2016, 9:00:00 am BST");
   })
